cd0f8e3a4b8dca2450c70687c79829ad23d17e39
VoTk4N <dikiaronzolo-95@live.it>
2025-01-03 15:02:45 +0100

    preload

---
+++ assets/js/preload.js
@@ -0,0 +1,5 @@
+
+window.addEventListener('load', function() { var preloader = document.getElementById('preloader'); 
+preloader.style.opacity = '0'; 
+setTimeout(function() { preloader.style.display = 'none'; }, 500); // Imposta il timeout in modo che coincida con la durata della transizione
+ });
